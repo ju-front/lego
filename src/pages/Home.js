7193cbe036d79a8ea2ca5902ebd2423ca8a7ba9a
김동욱 <bradl0809@naver.com>
2024-06-07 15:49:35 +0900

    feat: PathButton 추가 #3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,25 @@
-import { Hello } from 'ezy-ui';
-import { helloWorld } from 'ezy-fn';
+import { Card } from 'components/Card';
+import { PathButton } from 'components/PathButton';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>ezy-라이브러리 테스트</h1>
-      <Hello name="lego" />
-      <h1>{helloWorld()}</h1>
+    <div className="card-container">
+      <Card title="머신러닝" color="#4688F1" path="/machine-learning" />
+      <Card title="통계학및실습" color="#6DAEDD" path="/statistics" />
+      <Card title="SPA개발방법론" color="#AFDBF5" path="/spa-methods" />
+      <Card title="데이터베이스" color="#D6EAF8" path="/database" />
+      <PathButton
+        label="+"
+        path="/new-page"
+        color="#6DAEDD"
+        className="add-button"
+      />
+      <PathButton
+        label="테스트"
+        path="/minus-page"
+        color="#ff0000"
+        className="add-button"
+      />
     </div>
   );
 };
