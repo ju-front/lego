d7b3985bb0461c3e08981b5aea071973dfcb69b8
김동욱 <bradl0809@naver.com>
2024-06-07 17:59:42 +0900

    feat: 카드 및 생성 버튼 컴포넌트 적용 #3

--- src/pages/AttendanceListPage.js
+++ src/pages/AttendanceListPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Sidebar from 'components/Sidebar';
 import HeaderNav from 'components/HeaderNav';
+import { Card } from 'components/Card';
+import { PathButton } from 'components/PathButton';
 
 /**
  * @returns {JSX.Element} 출석 페이지 대시보드
@@ -20,7 +22,56 @@ const AttendanceListPage = ({ role }) => {
       <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         <HeaderNav title={title} />
         <div style={{ flex: 1, backgroundColor: 'brown' }}>
-          <h1>출석 페이지 대시보드</h1>
+          {role === 'Teacher' ? (
+            <div>
+              <h1>교수용 대시보드</h1>
+              <div className="card-container">
+                <Card
+                  title="SPA개발방법론"
+                  color="skyblue"
+                  path="/Attendance-Check"
+                />
+                <Card
+                  title="통계학 및 실습"
+                  color="lightgreen"
+                  path="/Attendance-Check"
+                />
+                <Card
+                  title="머신러닝"
+                  color="lightcoral"
+                  path="/Attendance-Check"
+                />
+              </div>
+              <PathButton
+                label="+"
+                path="/Class-Create"
+                color="#6DAEDD"
+                className="add-button"
+              />
+              {/* 교수용 대시보드 관련 콘텐츠 */}
+            </div>
+          ) : (
+            <div>
+              <h1>학생용 대시보드</h1>
+              <div className="card-container">
+                <Card
+                  title="SPA개발방법론"
+                  color="skyblue"
+                  path="/Attendance-Check"
+                />
+                <Card
+                  title="통계학 및 실습"
+                  color="lightgreen"
+                  path="/Attendance-Check"
+                />
+                <Card
+                  title="머신러닝"
+                  color="lightcoral"
+                  path="/Attendance-Check"
+                />
+              </div>
+            </div>
+          )}
           {/* 출석 페이지 대시보드 관련 콘텐츠 */}
         </div>
       </div>
