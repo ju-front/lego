9f08d3ebafd57cc305bb53884504b72de9d62676
김동욱 <bradl0809@naver.com>
2024-06-15 02:48:50 +0900

    feat: 수업방 상세 조회(서버 연동) #34

--- src/pages/CheckPage.js
+++ src/pages/CheckPage.js
@@ -13,42 +13,91 @@ import 'css/Timer.css';
  * @constructor
  */
 
-export const CheckPage = ({ role }) => {
+export const CheckPage = () => {
   /* 예시로 Teacher로 설정(설정은 router), 실제로는 사용자 정보를 기반으로 설정
    * user.role에 따라 Sidebar의 메뉴가 다르게 보여진다.
    * role은 Teacher, Student 두 가지로 구분한다.
    *
    * 출석 체크 방 생성페이지는 role에 따라 UI가 다르게 보여진다.
    */
-  const title =
-    role === 'Teacher' ? '교수용 출석 체크 페이지' : '학생용 출석 체크 페이지';
-
+  const title = '출석 체크 페이지';
   const { class_id } = useParams();
   const [classData, setClassData] = useState(null); // 수업 정보를 받아올 상태
+  const [userData, setUserData] = useState(null); // 사용자 정보를 받아올 상태
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [initialTime, setInitialTime] = useState(5 * 60); // 5분을 초 단위로 변환. 나중에 서버와 통신 필요
   const [isAttendanceStarted, setIsAttendanceStarted] = useState(false);
 
+  // 수업방 상세 조회
   useEffect(() => {
     const fetchClassData = async () => {
       try {
-        const response = await fetch('/dummyData.json');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken) {
+          throw new Error('Access token not found');
         }
-        const data = await response.json();
-        const classInfo = data.classes.find(
-          classItem => classItem.class_id === parseInt(class_id),
+
+        const response = await fetch(
+          `http://localhost:8080/api/classes/${class_id}`,
+          {
+            method: 'GET',
+            headers: {
+              access: accessToken,
+            },
+          },
         );
-        setClassData(classInfo);
-        setInitialTime(classInfo.late_time_limit * 60); // 출석 시간을 초 단위로 변환
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch class data');
+        }
+
+        const data = await response.json();
+        setClassData({
+          classId: data.classId,
+          className: data.className,
+          teacherName: data.teacherName,
+          lateTimeLimit: data.lateTimeLimit,
+          deskRows: data.deskRows,
+          deskColumns: data.deskColumns,
+        });
+        setInitialTime(data.lateTimeLimit * 60);
       } catch (error) {
-        console.error('Failed to load class data from dummy data', error);
+        console.error('Failed to load class data', error);
       }
     };
 
     fetchClassData();
   }, [class_id]);
+
+  // 유저 정보 로드 -> 프로필에 사용
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken) {
+          throw new Error('Access token not found');
+        }
+
+        const response = await fetch('http://localhost:8080/api/user', {
+          method: 'GET',
+          headers: {
+            access: accessToken,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error('Failed to load user data', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleStartClick = () => {
     setIsTimerRunning(true);
@@ -73,17 +122,17 @@ export const CheckPage = ({ role }) => {
     { path: '/sheet', label: '출결 현황' },
   ];
 
-  if (!classData) {
+  if (!classData || !userData) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="main-layout">
-      <Sidebar links={links} classId={class_id} />
+      <Sidebar links={links} userData={userData} classId={class_id} />
       <div className="main-content-container">
         <HeaderNav title={title} />
         <div className="main-content" style={{ backgroundColor: 'green' }}>
-          {role === 'Teacher' ? (
+          {userData.role === '선생' ? (
             <div>
               <h1>교수용 출석 체크 페이지</h1>
               <div className="timer-wrapper">
