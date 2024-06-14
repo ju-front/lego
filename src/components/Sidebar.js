53f031fd10dbe80c07d0145bb5e60384cd84ebde
김동욱 <bradl0809@naver.com>
2024-06-15 02:48:17 +0900

    feat: 사이드바에 userData 전달 #34

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import 'css/SideBarComponent.css';
 
 /**
  * 사이드바 공통 컴포넌트
  * @param {array} links - 사이드바에 표시할 링크 목록
+ * @param {object} userData - 사용자 정보
  * @returns {JSX.Element} 사이드바 영역
  * @constructor
  *
@@ -19,53 +20,9 @@ import 'css/SideBarComponent.css';
  * - 출석 체크 페이지
  * - 출결 현황
  */
-export const Sidebar = ({ links, classId }) => {
-  const [userData, setUserData] = useState(null);
+export const Sidebar = ({ links, userData, classId }) => {
   const location = useLocation();
-  const userId = 1; // 예시로 1로 설정
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/dummyData.json');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        const user = data.user.find(user => user.user_id === userId);
-        setUserData(user);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
-
-    fetchData();
-  }, [userId]);
-
-  // 실제로는 서버에서 사용자 정보를 받아온다.
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(`/api/user?user_id=${userId}`, {
-  //         method: 'GET',
-  //         headers: {
-  //           Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-  //         },
-  //       });
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
-  //       const data = await response.json();
-  //       const user = data.user; // 응답에서 사용자 데이터를 가져옵니다.
-  //       setUserData(user);
-  //     } catch (error) {
-  //       console.error('Error fetching user data:', error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [userId]);
 
   const handleLogout = () => {
     localStorage.removeItem('access_token');
@@ -74,7 +31,7 @@ export const Sidebar = ({ links, classId }) => {
 
   return (
     <div id="sidebar">
-      {userData && (
+      {userData && userData.name && (
         <div className="profile">
           <div className="profile-circle">{userData.name.charAt(0)}</div>
           <div className="profile-username">{userData.name}</div>
